0d227276a2b6507c4aee52feac6e7648bf67b3b7
Pratik Patil <48413868+PratikPatil131@users.noreply.github.com>
2023-10-19 10:17:25 +0530

    Update startservers.sh

--- startservers.sh
+++ startservers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #sh /apache-tomcat-9.0.74/bin/startup.sh
 #service apache2 start && tail -f /dev/null
-screen -dmS django && bash -c 'cd /django-todo-cicd && python manage.py makemigrations && python manage.py migrate && python manage.py runserver 0.0.0.0:8080'
+screen -dmS django && bash -c 'cd django-todo-cicd && python manage.py makemigrations && python manage.py migrate && python manage.py runserver 0.0.0.0:8080'
